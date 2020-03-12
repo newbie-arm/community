c76c1ff902f823f0d5bd8fe4d9806b6f80094798
arman <arman503@163.com>
2020-03-12 21:14:38 +0800

    UserService

--- src/main/java/com/newbie/community/service/UserService.java
+++ src/main/java/com/newbie/community/service/UserService.java
@@ -3,7 +3,15 @@ package com.newbie.community.service;
 import com.newbie.community.model.User;
 
 public interface UserService {
+    /**
+    添加用github登陆的用户
+     */
     void addUser(User user);
 
+    /**
+     * 用token查找用户
+     * @param token
+     * @return
+     */
     User findByToken(String token);
 }
