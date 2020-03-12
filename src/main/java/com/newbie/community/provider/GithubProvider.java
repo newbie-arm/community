c7caf78605a26c4e94dd9dcf3fbe5d69cf148db6
arman <arman503@163.com>
2020-03-12 13:25:10 +0800

    用户Github登陆模块

---
+++ src/main/java/com/newbie/community/provider/GithubProvider.java
@@ -0,0 +1,57 @@
+package com.newbie.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.newbie.community.dto.AccessTokenDto;
+import com.newbie.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDto accessTokenDto){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String str = response.body().string();
+            // access_token=adbb9c4607be006ab9e3b4c9328641058d573f5a&scope=&token_type=bearer
+            // 需要截取 =  和 & 中间的 token
+            String[] split = str.split("&");
+            String[] tokenSplit = split[0].split("=");
+            String token = tokenSplit[1];
+            System.out.println(token);
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public GithubUser getUser(String token) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+token)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String str = response.body().string();
+            GithubUser githubUser = JSON.parseObject(str, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
